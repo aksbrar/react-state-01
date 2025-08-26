1e2f9bad40288f42687e217581ecb7345635b26a
aksbrar <aksbrar34@gmail.com>
2025-08-25 22:17:19 -0400

    made it work

--- src/comp/Section.jsx
+++ src/comp/Section.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react"
+
 export default function Section(){
+
+  // incredients list
+  let [ingredientsList, setIngredientsList] = useState([])
+
+  function handleSubmit(event) {
+        event.preventDefault()
+        alert("working")
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngredientsList(ingredientsList => [...ingredientsList, newIngredient] )
+  }
+
   return (
     <section>
-      <form className="add-ingredient-form">
+      <form onSubmit={handleSubmit} className="add-ingredient-form">
         <input 
             type="text"
             placeholder="e.g. oregano"
             aria-label="Add ingredient"
+            name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
+
+      <ul className="list-container">
+        {ingredientsList.map((item)=> {return <li key={item}>{item}</li>})}
+      </ul>
     </section>
   )
 }

f875a93d96375e2c10287e4f0d9cb5a5229aebf5
aksbrar <aksbrar34@gmail.com>
2025-08-28 19:34:35 -0400

    conditional render only if incredients more tha 3

--- src/comp/Section.jsx
+++ src/comp/Section.jsx
@@ -30,13 +30,13 @@ export default function Section(){
         <section>
             <h2>Ingredients on hand:</h2>
             <ul className="ingredients-list" aria-live="polite">{ingredientsListHtml}</ul>
-            <div className="get-recipe-container">
+            {ingredientsList.length < 3 && <div className="get-recipe-container">
                 <div>
                     <h3>Ready for a recipe?</h3>
                     <p>Generate a recipe from your list of ingredients.</p>
                 </div>
-                <button>Get a recipe</button>
-            </div>
+                <button >Get a recipe</button>
+            </div>}
         </section>
       }
       

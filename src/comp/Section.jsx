e65a6977411c2b9e068b06871d52e417b621de42
aksbrar <aksbrar34@gmail.com>
2025-08-29 12:40:33 -0400

    making incredients its on componenet

--- src/comp/Section.jsx
+++ src/comp/Section.jsx
@@ -1,13 +1,11 @@
 import { useState } from "react"
+import Incredients from "./Incredients.jsx"
 
 export default function Section(){
 
   // states
-  let [ingredientsList, setIngredientsList] = useState(["all spices", "oregano", "chicken", "beef"])
   let [isShown, setisShown] = useState(false)
-
-  // incredient lists
-  let ingredientsListHtml = ingredientsList.map((item)=> {return <li key={item}>{item}</li>})
+  let [ingredientsList, setIngredientsList] = useState(["all spices", "oregano", "chicken", "beef"])
 
   // function to handleform
   function handleForm(formData) {
@@ -33,22 +31,7 @@ export default function Section(){
         <button>Add ingredient</button>
       </form>
 
-      {
-        ingredientsList.length > 0 &&
-        <section>
-            <h2>Ingredients on hand:</h2>
-            <ul className="ingredients-list" aria-live="polite">{ingredientsListHtml}</ul>
-            {
-              ingredientsList.length > 3 &&
-              <div className="get-recipe-container">
-                <div>
-                    <h3>Ready for a recipe?</h3>
-                    <p>Generate a recipe from your list of ingredients.</p>
-                </div>
-                <button onClick={showRecpie}>{isShown ? "Hide" : "Show"} recipe</button>
-              </div>}
-        </section>
-      }
+      <Incredients ingredientsList={ingredientsList}/>
 
       {
         isShown && 

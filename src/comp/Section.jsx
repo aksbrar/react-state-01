f7478249d9b1ad0f4a76ca69b4f400d3d6a04a62
aksbrar <aksbrar34@gmail.com>
2025-08-29 13:07:06 -0400

    final refactor before pulling ai in

--- src/comp/Section.jsx
+++ src/comp/Section.jsx
@@ -5,25 +5,33 @@ import Form from "./Form"
 
 export default function Section(){
 
-  // incredients list
+  // state variables
   let [ingredientsList, setIngredientsList] = useState(["all spices", "oregano", "chicken", "beef"])
   let [isShown, setisShown] = useState(false)
+
+  // ingredients list
   let ingredientsListHtml = ingredientsList.map((item)=> {return <li key={item}>{item}</li>})
 
+  // handle form
   function handleForm(formData) {
         const newIngredient = formData.get("ingredient")
         setIngredientsList(ingredientsList => [...ingredientsList, newIngredient] )
   }
 
+  // show reipie
   function showRecpie(){
     setisShown(prev => !prev) 
   }
 
+  // return component
   return (
     <main>
       <Form handleForm={handleForm}/>
 
-      <Ingredients isShown={isShown} ingredientsList={ingredientsList} showRecpie={showRecpie} ingredientsListHtml={ingredientsListHtml}/>
+      <Ingredients isShown={isShown}
+                   ingredientsList={ingredientsList} 
+                   showRecpie={showRecpie} 
+                   ingredientsListHtml={ingredientsListHtml}/>
 
       <Recipe isShown={isShown} />
     </main>
